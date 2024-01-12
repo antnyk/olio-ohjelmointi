883c33f12e8cff63c2ea25ac64c60c850bd44f81
Antti <anttipnykanen@gmail.com>
2024-01-12 18:36:33 +0200

    Homework part 1 finished

--- th1testisubdirs/osa1/main.cpp
+++ th1testisubdirs/osa1/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+//toimii ilman <ctime>:a, mutta miksi?
 
 using namespace std;
 
 int main()
 {
-    cout << "Terve maailma!" << endl;
+    srand(time(0));
+    int randomLuku = rand() % 20 + 1;
+    int pelaajanArvaamaLuku;
+    do{
+        cout << "Guess number between 20-1:";
+        cin >> pelaajanArvaamaLuku;
+        if (pelaajanArvaamaLuku < randomLuku){
+            cout << "Number is bigger" << endl;
+        }
+        else if (pelaajanArvaamaLuku > randomLuku){
+            cout << "Number is smaller" << endl;
+        }
+    }while(pelaajanArvaamaLuku != randomLuku);
+    cout << "Correct!" << endl;
+
     return 0;
 }
