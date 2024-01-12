1da23b55de5b0f1c9deccc2eaa2cb396566b0e17
Antti Nykanen <anttipnykanen@gmail.com>
2024-01-12 14:01:12 +0200

    removed th1 folder from been tracked

---
+++ th1testisubdirs/osa1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Terve maailma!" << endl;
+    return 0;
+}
---
+++ th1testisubdirs/th1testisubdirs.pro
@@ -0,0 +1,6 @@
+TEMPLATE = subdirs
+
+SUBDIRS += \
+    osa2 \
+    osa1
+
