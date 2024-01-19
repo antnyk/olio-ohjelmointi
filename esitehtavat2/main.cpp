692279c0912676b174d580dc0ad6fdbf30263d6f
Antti Nykanen <anttipnykanen@gmail.com>
2024-01-19 12:40:25 +0200

    harjoitustehtava 3 DONE

--- esitehtavat2/main.cpp
+++ esitehtavat2/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vehicle.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //th3
+    Vehicle v;
+    v.setName("Ford");
+    v.setSpeed(85);
+    cout<<v.getName()+" , ";
+    cout<<v.getSpeed();
+    cout<<" km/h"<<endl;
+
+
+
     return 0;
 }
