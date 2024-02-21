7ceda2a83619e10fad29a740afef8422914f9648
Antti Nykanen <anttipnykanen@gmail.com>
2024-02-21 18:45:08 +0200

    a

--- tehtava7/mainwindow.h
+++ tehtava7/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+
+#define TWO_MIN 120000;
+#define FIVE_MIN 300000;
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +22,9 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QTimer *objectTimer;
+    short x=0;
+
+
 };
 #endif // MAINWINDOW_H
