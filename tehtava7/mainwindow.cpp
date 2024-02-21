1baac133a59112f0fd160dbea4e305d410dece89
Antti <anttipnykanen@gmail.com>
2024-02-21 21:37:44 +0200

    Base is basicly done

--- tehtava7/mainwindow.cpp
+++ tehtava7/mainwindow.cpp
@@ -7,10 +7,132 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     objectTimer=new QTimer();
+
+    ui->btnStart->setEnabled(false);
+    ui->btnContinue->setEnabled(false);
+    ui->btnStop->setEnabled(false);
+    ui->btnRestart->setEnabled(false);
+    ui->progbarPlayer1->setValue(0);
+    ui->progbarPlayer2->setValue(0);
+    ui->label->setText("SELECT PLAYTIME AND PRESS START GAME");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete objectTimer;
+    objectTimer=nullptr;
+}
+
+
+void MainWindow::on_btnStart_clicked()
+{
+    connect(objectTimer, SIGNAL(timeout()),this,SLOT(timePass()));
+    objectTimer->start(1000);
+    ui->label->setText("GAME ONGOING");
+    ui->btnStart->setEnabled(false);
+    ui->btnStop->setEnabled(true);
+    //time buttons need to be disabled
+    ui->btnTimeFiveMin->setEnabled(false);
+    ui->btnTimeTwoMin->setEnabled(false);
+    ui->btnRestart->setEnabled(true);
+}
+
+void MainWindow::timePass(){
+    switch (state){
+    case 0:
+        timeRemainingPlayer1 -= 1000;
+        qDebug()<<"Aikaa kulunut pelaaja1 "<<timeRemainingPlayer1/1000<<" sekunttia.";
+        ui->progbarPlayer1->setValue(timeRemainingPlayer1);
+        break;
+    case 1:
+        timeRemainingPlayer2 -= 1000;
+        qDebug()<<"Aikaa kulunut pelaaja2 "<<timeRemainingPlayer2/1000<<" sekunttia.";
+        ui->progbarPlayer2->setValue(timeRemainingPlayer2);
+        break;
+    }
+}
+
+void MainWindow::on_btnTimeTwoMin_clicked()
+{
+    ui->btnStart->setEnabled(true);
+    ui->label->setText("READY TO PLAY");
+    qDebug()<<"Time mode 2";
+    timeRemainingPlayer1 = TWO_MIN;
+    timeRemainingPlayer2 = TWO_MIN;
+    ui->progbarPlayer1->setMaximum(TWO_MIN);
+    ui->progbarPlayer2->setMaximum(TWO_MIN);
+    ui->progbarPlayer1->setValue(TWO_MIN);
+    ui->progbarPlayer2->setValue(TWO_MIN);
+}
+
+
+void MainWindow::on_btnTimeFiveMin_clicked()
+{
+    ui->btnStart->setEnabled(true);
+    ui->label->setText("READY TO PLAY");
+    qDebug()<<"Time mode 5";
+    timeRemainingPlayer1 = FIVE_MIN;
+    timeRemainingPlayer2 = FIVE_MIN;
+    ui->progbarPlayer1->setMaximum(FIVE_MIN);
+    ui->progbarPlayer2->setMaximum(FIVE_MIN);
+    ui->progbarPlayer1->setValue(FIVE_MIN);
+    ui->progbarPlayer2->setValue(FIVE_MIN);
+}
+
+
+void MainWindow::on_btnSwitchToPlayer1_clicked()
+{
+    state = 0;
+}
+
+
+void MainWindow::on_btnSwitchToPlayer2_clicked()
+{
+    state = 1;
+}
+
+
+void MainWindow::on_btnStop_clicked()
+{
+    objectTimer->stop();
+    qDebug()<<"Game stopped";
+    ui->label->setText("GAME STOPPED");
+    ui->btnContinue->setEnabled(true);
+}
+
+void MainWindow::gameWinner(int s)
+{
+    objectTimer->stop();
+    ui->btnStart->setEnabled(false);
+
+    switch(s){
+    case 0:
+        ui->label->setText("PLAYER 1 WON");
+        break;
+    case 1:
+        ui->label->setText("PLAYER 2 WON");
+        break;
+    }
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+
+}
+
+
+void MainWindow::on_btnContinue_clicked()
+{
+    objectTimer->start();
+    qDebug()<<"Game continues";
+    ui->btnContinue->setEnabled(false);
+}
+
+
+void MainWindow::on_btnRestart_clicked()
+{
+
 }
 
--- tehtava7/mainwindow.h
+++ tehtava7/mainwindow.h
@@ -4,8 +4,6 @@
 #include <QMainWindow>
 #include <QTimer>
 
-#define TWO_MIN 120000;
-#define FIVE_MIN 300000;
 
 
 QT_BEGIN_NAMESPACE
@@ -20,11 +18,38 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void timePass();
+
+    void on_btnStart_clicked();
+
+    void on_btnTimeTwoMin_clicked();
+
+    void on_btnTimeFiveMin_clicked();
+
+    void on_btnSwitchToPlayer1_clicked();
+
+    void on_btnSwitchToPlayer2_clicked();
+
+    void on_btnStop_clicked();
+
+    void on_pushButton_clicked();
+
+    void on_btnContinue_clicked();
+
+    void on_btnRestart_clicked();
+
 private:
     Ui::MainWindow *ui;
     QTimer *objectTimer;
-    short x=0;
+    int timeRemainingPlayer1=0;
+    int timeRemainingPlayer2=0;
 
+    const int TWO_MIN = 120000;
+    const int FIVE_MIN = 300000;
 
+    int state = 0;
+
+    void gameWinner(int s);
 };
 #endif // MAINWINDOW_H
